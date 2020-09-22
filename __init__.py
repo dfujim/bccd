e39686090d1ccaf53d84a6215576afa1b100059c
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-09-22 14:19:57 -0700

    Setting up for pypi

--- __init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ['fits','functions']
-__version__ = '1.0.0'
-__author__ = 'Derek Fujimoto'
-
-from bccd.fits import fits
-
---
+++ source/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['gui','backend']
+__version__ = '2.0.0'
+__author__ = 'Derek Fujimoto'
+
+from bccd.backend.fits import fits
+

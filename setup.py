f7d64a5ade873cf87fce0e699f254c8e20729225
Derek Fujimoto <fujimoto@phas.ubc.ca>
2021-08-14 21:03:26 -0700

    v2.8.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bccd",
-    version="2.8.2",
+    version="2.8.3",
     author="Derek Fujimoto",
     author_email="fujimoto@phas.ubc.ca",
     description="B-NMR/B-NQR Beamspot Image Viewer and Analysis",
